264c6a39e9bed6e188c4b0786282c5a7e9dc2568
MuhmdBilal <muhmdbilal3333@gmail.com>
2026-01-03 16:24:06 +0500

    api update

--- src/index.js
+++ src/index.js
@@ -14,6 +14,18 @@ connectDB();
 app.use("/api/admin", adminRoutes);
 app.use("/api/properties", propertyRoutes );
 app.use("/api/broker", brokerRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK");
+});
+
+// Health check route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    uptime: process.uptime(),
+  });
+});
 // const PORT = process.env.PORT || 8080;
 // app.listen(PORT, "0.0.0.0", () => {
 //   console.log(`Server running on port ${PORT}`);
